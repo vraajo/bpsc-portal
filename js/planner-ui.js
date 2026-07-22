abf2435e4a3a09fd7118e5ae4d134a3a7e089dc1
vraajo <45547568+vraajo@users.noreply.github.com>
2026-07-23 02:03:29 +0530

    Update planner-ui.js

--- js/planner-ui.js
+++ js/planner-ui.js
@@ -52,10 +52,110 @@ const PlannerUI = {
 
     },
 
+
+   //render art added in second time
+
     render() {
 
-        // Will build in next step
+    const container =
+        document.getElementById("plannerContainer");
+
+    if (!container) return;
+
+    const planner =
+        PlannerEngine.getPlanner();
+
+    if (
+        !planner ||
+        planner.subjects.length === 0
+    ) {
+
+        container.innerHTML = `
+
+        <div class="plannerEmpty">
+
+            <div class="plannerIcon">📚</div>
+
+            <h3>No Study Plan Yet</h3>
+
+            <p>Add your first subject to begin today's mission.</p>
+
+            <button
+                class="primary-btn"
+                id="addSubjectBtn">
+
+                + Add Subject
+
+            </button>
+
+        </div>
+
+        `;
+
+        this.bindEvents();
+
+        return;
 
     }
+
+    let html = "";
+
+    planner.subjects.forEach(subject => {
+
+        html += `
+
+        <div class="planner-card">
+
+            <div class="planner-title">
+
+                ${subject.title}
+
+            </div>
+
+            <div class="planner-topics">
+
+                ${subject.topics.map(topic => `
+
+                    <div class="planner-topic">
+
+                        ☐ ${topic.title}
+
+                    </div>
+
+                `).join("")}
+
+            </div>
+
+            <button
+                class="primary-btn addTopicBtn"
+                data-id="${subject.id}">
+
+                + Add Topic
+
+            </button>
+
+        </div>
+
+        `;
+
+    });
+
+    html += `
+
+    <button
+        class="primary-btn"
+        id="addSubjectBtn">
+
+        + Add Subject
+
+    </button>
+
+    `;
+
+    container.innerHTML = html;
+
+    this.bindEvents();
+
+}
 
 };
