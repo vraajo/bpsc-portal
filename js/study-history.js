6b7f2ee004ecfd018b53caa5d2c6f4217f72edc5
vraajo <45547568+vraajo@users.noreply.github.com>
2026-07-24 22:57:47 +0530

    Create study-history.js

---
+++ js/study-history.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const StudyHistory = {
+
+    init() {
+
+        console.log("Study History Initialized");
+
+    }
+
+};
+
+document.addEventListener(
+
+    "DOMContentLoaded",
+
+    () => {
+
+        StudyHistory.init();
+
+    }
+
+);
