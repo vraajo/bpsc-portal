a7957c3f0f03a44d18f468991fa8bcbbbd226601
vraajo <45547568+vraajo@users.noreply.github.com>
2026-07-26 23:57:45 +0530

    Update home-ui.js

--- js/home-ui.js
+++ js/home-ui.js
@@ -1,1 +1,101 @@
+/* ==========================================
+   Home UI
+========================================== */
 
+"use strict";
+
+const HomeUI = {
+
+    init() {
+
+        this.renderStudyTimer();
+
+    },
+
+    renderStudyTimer() {
+
+        const container =
+            document.getElementById(
+                "homeStudyTimer"
+            );
+
+        if (!container) return;
+
+        container.innerHTML = `
+
+<div class="studyTimerCard">
+
+    <div class="studyTimerHeader">
+
+        <span class="material-symbols-rounded">
+
+            timer
+
+        </span>
+
+        <h3>
+
+            Study Timer
+
+        </h3>
+
+    </div>
+
+    <div
+        class="studyTimerClock"
+        id="studyTimerClock">
+
+        00:00:00
+
+    </div>
+
+    <div class="studyTimerButtons">
+
+        <button
+            id="timerStartBtn">
+
+            <span class="material-symbols-rounded">
+
+                play_arrow
+
+            </span>
+
+            Start
+
+        </button>
+
+        <button
+            id="timerPauseBtn">
+
+            <span class="material-symbols-rounded">
+
+                pause
+
+            </span>
+
+            Pause
+
+        </button>
+
+        <button
+            id="timerStopBtn">
+
+            <span class="material-symbols-rounded">
+
+                stop
+
+            </span>
+
+            Stop
+
+        </button>
+
+    </div>
+
+</div>
+
+`;
+
+    }
+
+};
