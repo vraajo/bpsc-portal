7d829949fd19a17f040c0c4bc6cdf428cd798bc9
vraajo <45547568+vraajo@users.noreply.github.com>
2026-07-26 23:37:49 +0530

    Update home-engine.js

--- js/home-engine.js
+++ js/home-engine.js
@@ -7,6 +7,7 @@
 const HomeEngine = {
 
     data: null,
+   interval: null,
 
     init() {
 
@@ -30,6 +31,86 @@ const HomeEngine = {
 
         this.data = HomeStorage.load();
 
+    },
+
+   startTimer() {
+
+    if (this.data.timerState === "running") {
+
+        return;
+
     }
+
+    this.data.timerState = "running";
+
+    this.data.sessionStartTime = Date.now();
+
+    this.startTicker();
+
+    this.save();
+
+},
+
+pauseTimer() {
+
+    if (this.data.timerState !== "running") {
+
+        return;
+
+    }
+
+    clearInterval(this.interval);
+
+    this.interval = null;
+
+    this.data.timerState = "paused";
+
+    this.data.pausedAt = Date.now();
+
+    this.save();
+
+},
+
+resumeTimer() {
+
+    if (this.data.timerState !== "paused") {
+
+        return;
+
+    }
+
+    this.data.timerState = "running";
+
+    this.data.sessionStartTime =
+
+        Date.now() -
+
+        this.data.currentSessionSeconds * 1000;
+
+    this.startTicker();
+
+    this.save();
+
+},
+
+   startTicker() {
+
+    clearInterval(this.interval);
+
+    this.interval = setInterval(() => {
+
+        this.data.currentSessionSeconds = Math.floor(
+
+            (Date.now() -
+
+            this.data.sessionStartTime) / 1000
+
+        );
+
+        this.save();
+
+    },1000);
+
+},
 
 };
