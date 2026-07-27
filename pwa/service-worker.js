e568b4fcd52aab6110b0178b28d6c50f7b2ce766
vraajo <45547568+vraajo@users.noreply.github.com>
2026-07-27 23:13:59 +0530

    Update service-worker.js

--- pwa/service-worker.js
+++ pwa/service-worker.js
@@ -1,1 +1,45 @@
+const CACHE_NAME = "path-to-bpsc-v1";
 
+const urlsToCache = [
+  "/",
+  "/offline.html"
+];
+
+// Install
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request).then((response) => {
+        return response || caches.match("/offline.html");
+      });
+    })
+  );
+});
