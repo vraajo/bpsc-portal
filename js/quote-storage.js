0bb9e63c01db1d2e08cf45f82abbc5e0bf3d4ddc
vraajo <45547568+vraajo@users.noreply.github.com>
2026-08-05 14:43:40 +0530

    Create quote-storage.js

---
+++ js/quote-storage.js
@@ -0,0 +1,38 @@
+/* ==========================================
+   Quote Storage
+========================================== */
+
+"use strict";
+
+const QuoteStorage = {
+
+    quotes: [
+
+        {
+            text: "Dream, dream, dream. Dreams transform into thoughts and thoughts result in action.",
+            author: "Dr. A. P. J. Abdul Kalam"
+        },
+
+        {
+            text: "The future depends on what you do today.",
+            author: "Mahatma Gandhi"
+        },
+
+        {
+            text: "Success is the sum of small efforts repeated day in and day out.",
+            author: "Robert Collier"
+        },
+
+        {
+            text: "The expert in anything was once a beginner.",
+            author: "Helen Hayes"
+        },
+
+        {
+            text: "Do not wait for opportunity. Create it.",
+            author: "George Bernard Shaw"
+        }
+
+    ]
+
+};
